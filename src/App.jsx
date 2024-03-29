6e499f0b999b8ac49eadbca4b5406cd3a3842002
zahraa Meky <eng.alzahraa.meky@gmail.com>
2024-03-29 23:25:57 +0400

    Update App.jsx
    create layout of the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline bg-slate-700">
-      Hello world!
-    </h1>
+const App = ()=>
+  (
+    <main className="relative">
+      Nav
+      <section className="xl:padding-l wide:padding-r padding-b">
+        Hero
+      </section>
+      <section className="padding">
+        popular products
+      </section>
+      <section className="padding">
+        super Qulaity
+      </section>
+      <section className="padding padding-x py-10">
+       services
+      </section>
+      <section className="padding">
+        special offer
+      </section>
+      <section className="padding bg-pale-blue">
+        customer review
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+       subscrie
+      </section>
+      <section className="bg-black padding-x padding-t pb-8">
+       footer
+      </section>
+    </main>
   )
-}
+export default App
