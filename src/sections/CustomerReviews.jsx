b9eb4f085d1da1cb963181b6d3059ad25c1ccc30
zahraa Meky <eng.alzahraa.meky@gmail.com>
2024-03-30 22:00:16 +0400

    finish Customer Reviews section

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,3 +1,4 @@
+import ReviewCard from "../components/ReviewCard"
 import { reviews } from "../constants"
 
 function CustomerReviews() {
@@ -11,9 +12,9 @@ function CustomerReviews() {
         Hear genuine stories from our satisfied customers about their
         exceptional experiences with us.
       </p>
-      <div>
-      {reviews.map((review, index) => (
-
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+      {reviews.map((review) => (
+        <ReviewCard key={review.customerName} {...review}/>
     ))}
       </div>
     </section>
