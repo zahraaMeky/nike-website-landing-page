37382452bc7f0b8975ef8d720553270702e5a199
zahraa Meky <eng.alzahraa.meky@gmail.com>
2024-03-31 00:27:10 +0400

    Update Subscribe.jsx

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,8 +1,19 @@
+import Button from "../components/Button"
+
 function Subscribe() {
   return (
-    <div>
-      
-    </div>
+    <section id="contact-us" className="max-container flex  justify-between items-center gap-10 max-lg:flex-col">
+      <h3 className="text-4xl leading-[68px] font-palanquin lg:max-w-md font-bold">
+        Sign Up Form
+        <span className="text-coral-red">Updates </span> & Newsletter
+      </h3>
+      <div className="lg:max-w-[40%] w-full flex item-center max-sm:flex-col gap-5 p-2.5 sm:border-slate-gray rounded-full">
+        <input  className="input" type="text" placeholder="Subscribe@nike.com"/>
+        <div className="flex max-sm:justify-end items-center max-sm:w-full">
+          <Button label="Sign Up" fullWidth/>
+        </div>
+      </div>
+    </section>
   )
 }
 
